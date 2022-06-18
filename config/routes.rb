32d4761e7d0baa67584060fa16508327d9c7f337
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-06-18 17:16:47 -0300

    fix: changed get to post in auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'login', to: 'authentication#login'
-  get 'signup', to: 'authentication#login'
+  post 'login', to: 'authentication#login'
+  post 'signup', to: 'authentication#login'
   resources :classrooms
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
