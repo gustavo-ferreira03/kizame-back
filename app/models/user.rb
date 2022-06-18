92dbf36615b1da029943223f226bae93f3c8bb9a
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-06-18 16:30:12 -0300

    feat: added secure pass to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_and_belongs_to_many :classrooms
     has_many :practices, through: :practice_attendencies
     has_one :modality, through: :user_modality
+
+    has_secure_password
 end
