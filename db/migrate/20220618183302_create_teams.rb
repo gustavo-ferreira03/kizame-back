7206e057821c9b70185dc21347b54df8afc7c651
Rafaela Peçanha <rafaela.pecanha@injunior.com.br>
2022-06-18 16:24:54 -0300

    model team criado

---
+++ db/migrate/20220618183302_create_teams.rb
@@ -0,0 +1,12 @@
+class CreateTeams < ActiveRecord::Migration[7.0]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.string :name_teacher
+      t.date :day_week
+      t.time :time
+
+      t.timestamps
+    end
+  end
+end
