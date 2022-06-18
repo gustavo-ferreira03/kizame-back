775afb2d35d035b32dc8b28fb94df332f54a0e09
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-06-18 19:51:29 -0300

    feat: added instructor and students functions to classroom model

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,7 +1,15 @@
 class Classroom < ApplicationRecord
-    has_and_belongs_to_many :users
+    has_and_belongs_to_many :members, class_name: "User"
     has_one :classroom_modality
     has_one :modality, through: :classroom_modalities
 
     validates :name, presence: true
+
+    def instructor
+        members.find_by(role: "instructor") || nil
+    end
+
+    def students
+        members.where(role: "student")
+    end
 end
