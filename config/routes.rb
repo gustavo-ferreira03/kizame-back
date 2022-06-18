32525f225655594a9adb3226ddc511a00d558959
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-06-18 16:56:35 -0300

    feat: added new seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'authentication/login'
+  get 'authentication/signup'
+  get 'login/signup'
   resources :classrooms
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+def create_user(args)
+    user = User.create(args)
+end
+
+def create_modality(args)
+    capacity = Capacity.create(args)
+end
+
+def create_classroom(args)
+    classroom = Classroom.create(args)
+end
+
+User.delete_all
+puts "Creating Users"
+create_user(
+    name: "Gustavo",
+    last_name: "Ferreira",
+    email: "gustavo.ferreira@injunior.com.br",
+    password: "123",
+    password_confirmation: "123",
+    profile_picture_link: "teste"
+    role: 1
+)
+
+Capacity.delete_all
+puts "Creating capacities"
+modalities = ["Karatê", "jiu-jitsu", "krav maga", "zumba", "muay thai", "boxe")]
+modalities.each do |t|
+    create_modality(name: t)
+end
+
+Classroom.delete_all
+create_classroom(
+    name: "turma do gustavao"
+)
