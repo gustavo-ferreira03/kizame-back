05dcb6b0511b094406f3b1cae0f070887a7b3285
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-06-19 00:17:33 -0300

    fix: fixed user serializer modalities

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :last_name, :email, :profile_picture_link, :role
-  attribute :modalities
+  attribute :modality
   attribute :classrooms
 end
