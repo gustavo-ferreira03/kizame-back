42f34c407553c5493a4cf58b2a1c8cfafc904705
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-06-19 00:18:01 -0300

    feat: added instructor and students to classroom serializer

--- app/serializers/classroom_serializer.rb
+++ app/serializers/classroom_serializer.rb
@@ -1,6 +1,16 @@
 class ClassroomSerializer < ActiveModel::Serializer
   attributes :id, :name
   attribute :modality_name
+  attribute :instructor_name
+  attribute :students
+  
+  def instructor_name
+    "#{self.object.instructor.name} #{self.object.instructor.last_name}"
+  end
+
+  def students
+    self.object.students.map { |t| t.id }
+  end
 
   def modality_name
     self.object.modality.name
