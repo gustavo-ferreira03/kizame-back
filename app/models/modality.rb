a6525fe3aeb3ef0547179409582bbf7cfbf26861
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-06-18 23:12:28 -0300

    added has_many through associations to modality

--- app/models/modality.rb
+++ app/models/modality.rb
@@ -1,4 +1,7 @@
 class Modality < ApplicationRecord
     has_many :classroom_modalities
     has_many :user_modalities
+    
+    has_many :classrooms, through: :classroom_modalities
+    has_many :users, through: :users_modalities
 end
