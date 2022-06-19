039c2913aedb9ef4c040fa80e1cf17fdd45449e9
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-06-19 03:07:13 -0300

    feat: added practice serializer

---
+++ app/serializers/practice_serializer.rb
@@ -0,0 +1,3 @@
+class PracticeSerializer < ActiveModel::Serializer
+  attributes :id, :day, :qr_code_string
+end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :last_name, :email, :profile_picture_link, :role
   has_one :modality
   has_many :classrooms
+  has_many :practices
 end
