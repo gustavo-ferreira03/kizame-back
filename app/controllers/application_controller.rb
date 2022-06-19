e9116b22145239abef7a4ee24fbf12e4bff05959
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-06-19 00:18:21 -0300

    added verify_student to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::API
     render json: {message: "Permissão negada, usuário não está logado"}, status: 401 and return unless current_user.present?
   end
 
+  def verify_student
+    render json: {message: "Permissao Negada, usuário não é aluno"}, status: 401 and return unless current_user.student?
+  end
+
   def verify_instructor
     render json: {message: "Permissao Negada, usuário não é professor"}, status: 401 and return unless current_user.instructor?
   end
