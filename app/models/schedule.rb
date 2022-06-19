82d6f37661be80902a77bfbcb6c73d0e5d6c795f
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-06-19 14:59:13 -0300

    feat: added schedule model functions and validations

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,29 @@
 class Schedule < ApplicationRecord
   belongs_to :classroom
+
+  validates_presence_of :from_weekday, :to_weekday, :from_time, :to_time
+  validates :from_weekday, :to_weekday, inclusion: { in: 0..6 }
+  validates :from_time, :to_time, inclusion: { in: 0..23 }
+  validates :to_weekday, comparison: { greater_than: :from_weekday }
+  validates :to_time, comparison: { greater_than: :from_time }
+
+  def in_schedule?
+      if in_weekday? && in_time?
+        return true
+      else
+        return false
+      end
+  end
+
+  private
+
+    def in_weekday?
+      current_weekday = Time.now.wday
+      return (current_weekday >= from_weekday && current_weekday <= to_weekday)
+    end
+
+    def in_time?
+      current_time = Time.now
+      return (current_time >= from_time && current_time <= to_time)
+    end
 end
