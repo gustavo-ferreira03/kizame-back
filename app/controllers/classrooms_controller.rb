293e1996d0915966c80ec0fe1f14044c9902ec2c
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-06-19 00:19:00 -0300

    feat: added new routes for classroom controller

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,5 +1,5 @@
 class ClassroomsController < ApplicationController
-  before_action :set_classroom, only: [:show, :update, :destroy]
+  before_action :set_classroom, only: [:show, :update, :destroy, :join_classroom, :leave_classroom]
   before_action :verify_authenticated
   before_action :verify_instructor, only: [:create, :update]
   before_action :verify_admin, only: [:destroy]
@@ -19,10 +19,11 @@ class ClassroomsController < ApplicationController
 
   # POST /classrooms
   def create
-    @classroom = current_user.classrooms.build(classroom_params)
+    @classroom = Classroom.new(classroom_params)
     @classroom.build_classroom_modality(modality_id: user_modality.id)
-
+    
     if @classroom.save
+      @classroom.members.append(current_user)
       render json: @classroom, status: :created, location: @classroom
     else
       render json: @classroom.errors, status: :unprocessable_entity
@@ -43,6 +44,31 @@ class ClassroomsController < ApplicationController
     @classroom.destroy
   end
 
+  # GET /my_classrooms
+  def my_classrooms
+    @classrooms = current_user.classrooms
+
+    render json: @classrooms
+  end
+
+  # POST /classrooms/1/join
+  def join_classroom
+    if @classroom.add_user(current_user)
+      render json: current_user
+    else
+      render json: { message: "Usuário já está na turma" }, status: 403
+    end
+  end
+
+  # POST /my_classrooms/1/leave
+  def leave_classroom
+    if @classroom.remove_user(current_user)
+      render json: current_user
+    else
+      render json: { message: "Usuário não está na turma" }, status: 400
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_classroom
@@ -56,6 +82,10 @@ class ClassroomsController < ApplicationController
 
     def available_classrooms
       user_modality.classrooms
+    end
+
+    def verify_user_not_in_classroom
+
     end
 
     # Only allow a list of trusted parameters through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   post 'login', to: 'authentication#login'
   post 'signup', to: 'authentication#signup'
+
   resources :classrooms
+  get 'my_classrooms', to: 'classrooms#my_classrooms'
+  match 'classrooms/:id/join', to: 'classrooms#join_classroom', via: [:post]
+  match 'my_classrooms/:id/leave', to: 'classrooms#leave_classroom', via: [:post]
+  
   resources :users
   resources :modalities, except: [:show, :update]
-  get '/my_classrooms', to: 'modality#my_classrooms'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
